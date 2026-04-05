7c8a65e27e49cd22e389b9f9b98d327223f5fd1d
0zd0 <67220210+0zd0@users.noreply.github.com>
2026-04-06 02:40:00 +0400

    feat: bypass vpn check

--- patches/src/main/kotlin/app/template/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/template/patches/shared/Constants.kt
@@ -5,20 +5,18 @@ import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
 
 object Constants {
-    val COMPATIBILITY_EXAMPLE = Compatibility(
-        name = "XYZ app",
-        packageName = "com.example.app",
+    val COMPATIBILITY_CHMAX = Compatibility(
+        name = "Chmax",
+        packageName = "ru.oneme.app",
         apkFileType = ApkFileType.APK,
         appIconColor = 0xFF0045, // Icon color in Morphe Manager
         targets = listOf(
             // "version = null" means the patch works with the latest app target
             // and is expected to work with all future app targets
             AppTarget(
-                version = "2.0.0"
-            ),
-            AppTarget(
-                version = "1.0.2",
+                version = "26.10.1"
             )
         )
     )
+    const val IP_CHECKER = "https://ipv4-internet.yandex.net/api/v0/ip"
 }
